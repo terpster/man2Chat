fa108dd8fa9ee76ac9b46c48d1ac154a3ac70d60
terpster <ktn1710@gmail.com>
2017-05-05 20:12:47 +0200

    testing in action

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
-
+import {RouterModule} from "@angular/router";
+import { RouterTestingModule } from '@angular/router/testing';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [RouterModule, RouterTestingModule]
     })
     .compileComponents();
   }));
--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {FormsModule} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports:[FormsModule],
+      providers: [AuthService]
     })
     .compileComponents();
   }));
